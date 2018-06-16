1c04ab79d28fe4444964a12b3ec2210221ceab23
Elea DIOT <elea.diot@gmail.com>
2018-06-16 16:10:51 +0000

    exo week 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    
+    @name = 'Elea DIOT'
+    @surname_1 = 'aka justelea'
+    @surname_2 = 'juste Elea pour les intimes'
+    @description = 'Jeune codeuse en herbe'
+    @comment = '... ou pas lol'
+     
+ @curiosities = [
+   'Denis',
+   'Magnum',
+   'Barbecul team',
+   'Plage' ,
+   'Apero']
+   
   end
 end
